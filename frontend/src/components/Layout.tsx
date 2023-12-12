1ebe6472f9713dc63e888d11e7c9ffa207600dd4
KimKyeongSeog <kimks9241@naver.com>
2023-12-12 16:27:27 +0900

    Web3 Settings

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,14 +1,46 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import Web3, { Contract, ContractAbi } from "web3";
+import { useSDK } from "@metamask/sdk-react";
+
+import mintNftAbi from "../abis/mintNftAbi.json";
 import Header from "./Header";
+
+export interface OutletContext {
+  account: string;
+  web3: Web3;
+  mintNftContract: Contract<ContractAbi>;
+}
 
 const Layout: FC = () => {
   const [account, setAccount] = useState<string>("");
+  const [web3, setWeb3] = useState<Web3>();
+  const [mintNftContract, setMintNftContract] =
+    useState<Contract<ContractAbi>>();
+  //generic 안에 generic 가능
+  const { provider } = useSDK();
+
+  useEffect(() => {
+    if (!provider) return;
+
+    setWeb3(new Web3(provider));
+  }, [provider]);
+
+  useEffect(() => {
+    if (!web3) return;
+
+    setMintNftContract(
+      new web3.eth.Contract(
+        mintNftAbi,
+        "0x507a4eAb55b754DA1b0437914f02231c9ca0441e"
+      )
+    );
+  }, [web3]);
 
   return (
     <div className="bg-red-100 min-h-screen mx-auto">
       <Header account={account} setAccount={setAccount} />
-      <Outlet />
+      <Outlet context={{ account, web3, mintNftContract }} />
     </div>
   );
 };
