98012a10a7388a6bd2f665b8efe364636fe522cd
KimKyeongSeog <kimks9241@naver.com>
2023-12-15 14:22:00 +0900

    functions done

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -86,7 +86,7 @@ const Home: FC = () => {
 
   return (
     <>
-      <div className="grow bg-green-100">
+      <div className="grow ">
         <ul className="p-8 grid grid-cols-2 gap-8">
           {metadataArray?.map((v, i) => (
             <NftCard
--- frontend/src/pages/sale.tsx
+++ frontend/src/pages/sale.tsx
@@ -1,11 +1,67 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import { NftMetadata, OutletContext } from "../types";
+import axios from "axios";
+import SaleNftCard from "../components/SaleNFTCard";
 
 const Sale: FC = () => {
-  return <div> Sale</div>;
+  const [metadataArray, setMetadataArray] = useState<NftMetadata[]>([]);
+
+  const { saleNftContract, mintNftContract } =
+    useOutletContext<OutletContext>();
+
+  const getSaleNFTs = async () => {
+    try {
+      const onSaleNFTs: bigint[] = await saleNftContract.methods
+        .getOnSaleNFTs()
+        .call();
+
+      let temp: NftMetadata[] = [];
+
+      for (let i = 0; i < onSaleNFTs.length; i++) {
+        const metadataURI: string = await mintNftContract.methods
+          // @ts-expect-error
+          .tokenURI(Number(onSaleNFTs[i]))
+          .call();
+
+        const response = await axios.get(metadataURI);
+
+        temp.push({ ...response.data, tokenId: Number(onSaleNFTs[i]) });
+      }
+
+      setMetadataArray(temp);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!saleNftContract) return;
+
+    getSaleNFTs();
+  }, [saleNftContract]);
+
+  useEffect(() => console.log(metadataArray), [metadataArray]);
+
+  return (
+    <div className="">
+      <div className="text-center py-8">
+        <h1 className="font-bold text-2xl">Sale NFTs</h1>
+      </div>
+      <ul className="p-8 grid grid-cols-2 gap-8">
+        {metadataArray?.map((v, i) => (
+          <SaleNftCard
+            key={i}
+            image={v.image}
+            name={v.name}
+            tokenId={v.tokenId!}
+            metadataArray={metadataArray}
+            setMetadataArray={setMetadataArray}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Sale;
-
-//사고팔 수 있는 페이지를 추가할 예정
-//스마트 컨트렉트의 강점을 살리기 위해 만들어보고자 함
-//판매중인 nft를 리스트화 한 후 판매 예정
